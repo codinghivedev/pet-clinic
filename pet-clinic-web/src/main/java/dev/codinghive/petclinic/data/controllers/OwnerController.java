b0af033c5085fc1115feeacaba98d5fb62c24e8c
Imran Bajwa <imranbajwa.app@gmail.com>
2020-12-07 15:17:54 +0500

    added owner index page and controller. closses #13

--- pet-clinic-web/src/main/java/dev/codinghive/petclinic/data/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/dev/codinghive/petclinic/data/controllers/OwnerController.java
@@ -4,9 +4,15 @@ import dev.codinghive.petclinic.data.services.OwnerService;
 import dev.codinghive.petclinic.data.services.map.OwnerServiceMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
-    @Autowired
-    OwnerServiceMap OwnerServiceMap;
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
 }
